016d5eaf82029c0e1334316f4000f0c95e3ae370
Usummon <breakcreak@gmail.com>
2019-09-17 18:15:19 -0700

    can only predict lines right now, pyplot makes it seem incorrect for some wierd reason

--- linear.py
+++ linear.py
@@ -1,3 +1,5 @@
+import numpy as np;
+import matplotlib.pyplot as plt;
 #error cost square function
     # equation of the line we create : y = mx + b
     # re-write the equation to create the error cost function: y = mx + b
@@ -17,17 +19,45 @@
     # hence our function also boils down to (prediction - target)^2
 
 
-def error_cost(m, b, x, y):
+def error_cost(m, b, x, y, learning_rate):
     #x and y aren't multiple values so the function has to take the sum of all the y and x inputs and divide by the average
-    prediction = np.sum(m*x + b) / x.size;
-    target = np.sum(y) / y.size;
+    
+    # we need to calculate the derivative because it not only tells us when to stop calculating 'm' and 'b' but also tells us off we are and guides our calculations
 
+    # calculate the partial derivative of m and the partial derivative of b and use it re-calculate m and b
+    # derivative with respect to x = 2(mx + b - y)x
+    while True:
+        m_derivative = 2 * (np.sum(x*(m*x + b - y)) / y.size)
+        b_derivative = 2 * (np.sum(m*x + b - y) / y.size)
 
-def predict():
-    pass
+        prev_m = m
+        prev_b = b
+
+        print(m, b)
+
+        m = m - (m_derivative * learning_rate)
+        b = b - (b_derivative * learning_rate)
+
+        print(m, b)
+
+        #the if statement should be based on how different it was from last time
+        if prev_m > (m - 1e-6) and prev_m < (m + 1e-6) and prev_b > (b - 1e-6) and prev_b < (b + 1e-6):
+            return m, b 
+        
 
 def main():
-    pass
+    m = np.random.randint(1, 100)
+    b = np.random.randint(1, 100)
+
+    x = np.array([1, 2, 3, 4])
+    y = np.array([2, 4, 6, 8])
+
+    (m, b) = error_cost(m, b, x, y, 0.0001);
+
+    plt.set_cmap("jet")
+    plt.scatter(x, y);
+    plt.plot(2*x);
+    plt.show();
 
 
 main()
